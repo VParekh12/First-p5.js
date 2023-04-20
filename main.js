8bfd199115b1a37c496fa1e631a05b153aa02063
VParekh12 <121426380+VParekh12@users.noreply.github.com>
2023-04-20 17:54:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function preload(){
+    
 }
 
 function setup() {
@@ -12,16 +13,43 @@ function setup() {
 
 function draw(){
     image(video, 0, 0, 640, 480);
-    tint(tint_color);
+      
+      fill(255, 0, 0);
+      stroke(0, 0, 255);
+      circle(50, 50, 75);
+
+      fill(255, 0, 0);
+      stroke(0, 0, 255);
+      circle(600, 50, 75);
+
+      fill(255, 0, 0);
+      stroke(0, 0, 255);
+      circle(50,430, 75);
+
+      fill(255, 0, 0);
+      stroke(0, 0, 255);
+      circle(600, 430, 75);
+
+      fill(0, 255, 0);
+      stroke(0, 0, 255);
+      rect(90, 40, 472, 20);
+
+      fill(0, 255, 0);
+      stroke(0, 0, 255);
+      rect(40, 80, 20, 320)
+
+      fill(0, 255, 0);
+      stroke(0, 0, 255);
+      rect(90, 420, 472, 20);
+
+      fill(0, 255, 0);
+      stroke(0, 0, 255);
+      rect(590, 80, 20, 320)
 }
 
 function take_snapshot(){
     save('Vivaan.png');
 }
 
-function filter_tint()
-{
-  tint_color = document.getElementById("color_name").value;
-}
 
 
